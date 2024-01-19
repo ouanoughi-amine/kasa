8d0246edab8cbd247905fd97f2d4cb402f1a9abe
ouanoughi <ouanoughiamine1995@gmail.com>
2024-01-19 22:17:23 +0100

    four commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Path from './src/Path.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    //**** */ quand on rajoute un truck a notre url la page not found saffiche*****
-    errorElement: <h1>not found</h1>
-  },
-  
-  {
-    path: "/apartment",
-    element: <div>appartment</div>,
-  },
-  {
-    path: "/about",
-    element: <div>about</div>,
-  },
-]);
 
+//renvoie le composant Path qui contient le routes.
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+      <Path />
+    </React.StrictMode>,
+  );
